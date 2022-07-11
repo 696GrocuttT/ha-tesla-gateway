531fa4043026be070d18eeb3aa40d179a6718abb
Thomas Grocutt <tom@dsoworld.co.uk>
2022-07-11 20:21:39 +0100

    Made set tariff function more relisiant

--- custom_components/tesla_gateway/__init__.py
+++ custom_components/tesla_gateway/__init__.py
@@ -109,24 +109,24 @@ def async_setup(hass, config):
 
 
 async def set_tariff(hass, battery, service):
-    rawTariff     = service.data['tariff_periods']
+    rawTariff = service.data['tariff_periods']
+    if rawTariff:
+        def parseRateStr(rateStr):
+            rateParts = rateStr.split(" ")
+            return (float(rateParts[0]), float(rateParts[1]), rateParts[2])
+        
+        secondsToTime = lambda s: time(hour=s//3600, minute=(s%3600)//60, second=(s%3600)%60)
+        periods       = []
+        for curRateStr in rawTariff:
+            curRate    = parseRateStr(curRateStr)
+            periodCost = teslapy.BatteryTariffPeriodCost(curRate[0], curRate[1], curRate[2])
+            for period in rawTariff[curRateStr]:
+                period = teslapy.BatteryTariffPeriod(periodCost, secondsToTime(period[0]), secondsToTime(period[1]))
+                periods.append(period)
     
-    def parseRateStr(rateStr):
-        rateParts = rateStr.split(" ")
-        return (float(rateParts[0]), float(rateParts[1]), rateParts[2])
-    
-    secondsToTime = lambda s: time(hour=s//3600, minute=(s%3600)//60, second=(s%3600)%60)
-    periods       = []
-    for curRateStr in rawTariff:
-        curRate    = parseRateStr(curRateStr)
-        periodCost = teslapy.BatteryTariffPeriodCost(curRate[0], curRate[1], curRate[2])
-        for period in rawTariff[curRateStr]:
-            period = teslapy.BatteryTariffPeriod(periodCost, secondsToTime(period[0]), secondsToTime(period[1]))
-            periods.append(period)
-
-    defCost    = parseRateStr(service.data['default_prices'])
-    defCost    = teslapy.BatteryTariffPeriodCost(defCost[0], defCost[1], defCost[2])
-    planName   = "Autogen @ {0:%H:%M}".format(datetime.now())
-    tariffDict = teslapy.Battery.create_tariff(defCost, periods, service.data['provider'], planName)
-    await hass.async_add_executor_job(battery.set_tariff, tariffDict)
+        defCost    = parseRateStr(service.data['default_prices'])
+        defCost    = teslapy.BatteryTariffPeriodCost(defCost[0], defCost[1], defCost[2])
+        planName   = "Autogen @ {0:%H:%M}".format(datetime.now())
+        tariffDict = teslapy.Battery.create_tariff(defCost, periods, service.data['provider'], planName)
+        await hass.async_add_executor_job(battery.set_tariff, tariffDict)
         
